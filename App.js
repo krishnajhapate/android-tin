34a385b9ece5c49b0dbd74b2a897fb412ab8bbb4
Krishna jhapate <krishnajhapate@Krishnas-MacBook-Air.local>
2021-11-14 11:56:12 +0530

    added chat screen

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './StackNavigator';
 
 export default function App() {
@@ -13,11 +11,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- StackNavigator.js
+++ StackNavigator.js
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
-import { View, Text } from 'react-native'
+import ChatScreen from './screens/ChatScreen'
 import HomeScreen from './screens/HomeScreen'
 
 
@@ -9,6 +9,7 @@ const Stack = createNativeStackNavigator()
 const StackNavigator = () => {
     return (
         <Stack.Navigator>
+            <Stack.Screen name="Chat" component={ChatScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
     )
